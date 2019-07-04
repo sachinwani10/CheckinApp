e62055653fc04235c342ad55901d92f2e5833e37
Sachin Wani <sachinravindra.wani@mavs.uta.edu>
2019-07-04 01:56:00 -0500

    testinf db

--- test.py
+++ test.py
@@ -11,3 +11,10 @@ cursor = connection.cursor()
 
 create_table = "CREATE TABLE users (id int, username text, password text)"
 cursor.execute(create_table)
+
+user = (1, 'sachin', 'asdf')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+connection.commit()
+connection.close()
