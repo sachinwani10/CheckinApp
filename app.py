4d1fa5612fe467bc56611ff9d7f1591ccdc3bbd2
Sachin Wani <sachinravindra.wani@mavs.uta.edu>
2019-07-05 16:18:12 -0500

    sqlALCHEMY

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from resourses.user import UserRegister
 from resourses.clerk import Clerk, ClerkTotalHours
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:sourcecode10@server/db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'sachin'
@@ -22,7 +23,3 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
     app.run(debug=True)
-
-# Next Steps:
-# 3. Create database Model in models packages
-# 4. Write resourse methods
--- resourses/user.py
+++ resourses/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
@@ -24,13 +23,8 @@ class UserRegister(Resource):
         data = UserRegister.parser.parse_args()
         if UserModel.find_by_username(data['username']):
             return {"message": "User " + data['username'] + " already exists"}, 400
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        insert_query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(insert_query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {'message': 'User created successfully'}, 201
