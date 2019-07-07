0cb4c3041e8a177571940dd45e542f8feece0438
Sachin Wani <sachinravindra.wani@mavs.uta.edu>
2019-07-06 22:28:32 -0500

    autherizationEnforced
    JWT tocken is required to access methods in checkin and checkout resourses

--- resourses/checkin.py
+++ resourses/checkin.py
@@ -1,7 +1,8 @@
-# https://stackoverflow.com/questions/26662702/what-is-the-datetime-format-for-flask-restful-parser
 from flask_restful import Resource, reqparse
 from models.session_sheet import SessionModel
 from datetime import datetime
+from models.user import UserModel
+from flask_jwt import jwt_required
 
 class Checkin(Resource):
     parser = reqparse.RequestParser()
@@ -9,9 +10,12 @@ class Checkin(Resource):
     parser.add_argument('username', type=str, required=True)
     parser.add_argument('timestamp_in', type=str, required=True)
 
+    @jwt_required()
     def post(self):
         data = Checkin.parser.parse_args()
-        _timestamp = datetime.strptime(data['timestamp_in'], '%Y-%d-%m %H:%M:%S.%f')
-        record = SessionModel(data['username'], _timestamp, None)
-        record.save_checkin()
-        return {"message": "checked in at " + data['timestamp_in']}
+        if UserModel.find_by_username(data['username']):
+            _timestamp = datetime.strptime(data['timestamp_in'], '%Y-%d-%m %H:%M:%S.%f')
+            record = SessionModel(data['username'], _timestamp, None)
+            record.save_checkin()
+            return {"message": "checked in at " + data['timestamp_in']}
+        return {"message": data['username'] + " is not a registerd user" }
--- resourses/checkout.py
+++ resourses/checkout.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource, reqparse
 from models.session_sheet import SessionModel
 from datetime import datetime
+from models.user import UserModel
+from flask_jwt import jwt_required
 
 class Checkout(Resource):
     parser = reqparse.RequestParser()
@@ -8,9 +10,12 @@ class Checkout(Resource):
     parser.add_argument('username', type=str, required=True)
     parser.add_argument('timestamp_out', type=str, required=True)
 
+    @jwt_required()
     def post(self):
         data = Checkout.parser.parse_args()
-        _timestamp = datetime.strptime(data['timestamp_out'], '%Y-%d-%m %H:%M:%S.%f')
-        record = SessionModel(data['username'], None, _timestamp)
-        record.save_checkout()
-        return {"message": "checked out at " + data['timestamp_out']}
+        if UserModel.find_by_username(data['username']):
+            _timestamp = datetime.strptime(data['timestamp_out'], '%Y-%d-%m %H:%M:%S.%f')
+            record = SessionModel(data['username'], None, _timestamp)
+            record.save_checkout()
+            return {"message": "checked out at " + data['timestamp_out']}
+        return {"message": data['username'] + " is not a registerd user"}
