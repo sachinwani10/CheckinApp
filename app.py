0c1b791a1f76f63489b737dd2680030ff1499473
Sachin Wani <sachinravindra.wani@mavs.uta.edu>
2019-07-08 14:31:22 -0500

    workingfunctionality

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Checkin, '/checkin') # POST (Body: username, timestamp_in)
 api.add_resource(Checkout, '/checkout') # POST (Body: username, timestamp_out)
-api.add_resource(TotalHours, '/totalhours/<string:username>') # GET
+api.add_resource(TotalHours, '/totalhours/<string:username>/<string:start_date>/<string:end_date>') # GET
 api.add_resource(UserRegister, '/register')
 # api.add_resource(Clerk, '/clerk/<string:username>')
 # api.add_resource(ClerkTotalHours, '/hours/<string:username>/<string:startDate>/<string:endDate>')
--- resourses/totalhours.py
+++ resourses/totalhours.py
@@ -1,13 +1,29 @@
 from models.time_sheet import TimeSheetModel as TS
 from flask_restful import Resource
+from datetime import datetime
 
 class TotalHours(Resource):
-    def get(self, username):
+    def get(self, username, start_date, end_date):
+        # modify start and end date to datetime
+        start_date = datetime.strptime(start_date, '%Y-%d-%m')
+        end_date = datetime.strptime(end_date, '%Y-%d-%m')
+        result = TS.find_in_db(username, start_date, end_date)
+
         res = []
-        result = TS.find_in_db(username)
+        hours_worked = []
         if result:
             for row in result:
-                res.append(row.json())
-            return {"result": res}
+                mydict = row.json()
+                mydict['hours/minutes'] = \
+                str(self.calculate_hours(row.timestamp_in, row.timestamp_out)[0]) \
+                + " Hours - " + \
+                str(self.calculate_hours(row.timestamp_in, row.timestamp_out)[1]) \
+                + " Minutes"
+                res.append(mydict)
+            return {"Days": res}
         message = {"None": "None"}
         return message
+
+    def calculate_hours(self, s_timestamp, e_timestamp):
+        hours = e_timestamp - s_timestamp
+        return divmod(hours.total_seconds(), 3600)
